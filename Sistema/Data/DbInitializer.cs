90dbbf99283a5d4a6f2658090aff39c53599607b
Jose Useche <jose11useche@hotmail.com>
2019-07-16 15:43:37 -0400

    Primer Cmmit para cargar los archivos a GitHub

---
+++ Sistema/Data/DbInitializer.cs
@@ -0,0 +1,34 @@
+﻿using Sistema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SistemaContext context)
+        {
+            context.Database.EnsureCreated(); //Con este comando creo la base de datos
+
+            //Verificar si existen registros en la tabla categoría
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+            var categorias = new Categoria[]
+            {
+                new Categoria{Nombre = "Programación", Descripcion = "Cursos de Programación", Estado = true},
+                new Categoria{Nombre = "Diseño Gráfico", Descripcion = "Cursos de Diseño Gráfico", Estado = true}
+            };
+
+            foreach (var categoria in categorias)
+            {
+                context.Categoria.Add(categoria);
+            }
+
+            context.SaveChanges(); 
+        }
+    }
+}
